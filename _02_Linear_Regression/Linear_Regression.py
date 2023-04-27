2426b9ab5eae86d62b9544ad167f8cc79ca36b20
hentuatua <131958368+hentuatua@users.noreply.github.com>
2023-04-27 23:55:48 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -11,11 +11,16 @@ except ImportError as e:
 
 def ridge(data):
     X,y = read_data()
+    tem = X [:,1] + X [:,2]
+    X = np.column_stack((X,tem))
+    tem = X [:,3] - X [:,4]
+    X = np.column_stack((X,tem))
     b = np.ones(X.shape[0])
     X = np.column_stack((X,b))
-    l = 0.1
-    I = np.eye(7)
-    data = np.append(data,1)
+    data.append(1)
+    l = 0.5
+    I = np.eye(9)
+    data = np.append(data,[data[1]+data[2],data[3]-data[4],1])
     weight = np.matmul(np.linalg.inv(np.matmul(X.T,X)+ l*I),np.matmul (X.T,y))
     return weight @ data
     
