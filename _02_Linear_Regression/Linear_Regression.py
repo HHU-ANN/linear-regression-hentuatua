91f028d4b108ec10db32620311d4eea0da1aec21
hentuatua <1443379615@qq.com>
2023-04-27 23:14:00 +0800

    第一次提交

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -8,13 +8,34 @@ except ImportError as e:
     os.system("sudo pip3 install numpy")
     import numpy as np
 
+
 def ridge(data):
-    pass
+    X,y = read_data()
+    b = np.ones(X.shape[0])
+    X = np.column_stack((X,b))
+    l = 0.5
+    I = np.eye(7)
+    weight = np.matmul(np.matmul(np.linalg.inv(np.matmul(X,X.T)+l * I), X),y)
+    return weight @ data
     
 def lasso(data):
-    pass
+    X,y = read_data()
+    b = np.ones(X.shape[0])
+    X = np.column_stack((X,b))
+    l = 0.5
+    a = 0.5
+    t = 100
+    w = np.zeros(7)
+    for i in range (t):
+        s = np.zeros(7)
+        for j in range (X.shape[0]-1):
+            s = s - np.matmul(X,y-np.matmul(X,w)) + l * sgn(w)
+        w = w - a / X.shape[0] * s
+
 
 def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
     y = np.load(path + 'y_train.npy')
     return x, y
+
+
