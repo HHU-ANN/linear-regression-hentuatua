50c843ada6490d5e6f64d6310746ef49e53940c6
hentuatua <131958368+hentuatua@users.noreply.github.com>
2023-04-28 12:10:32 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -26,7 +26,26 @@ def ridge(data):
     return weight @ data
     
 def lasso(data):
-    return ridge(data)
+    X,y = read_data()
+    tem = X [:,0] * X [:,1]
+    X = np.column_stack((X,tem))
+    tem = X [:,2] * X [:,3]
+    X = np.column_stack((X,tem))
+    tem = X [:,4] * X [:,5]
+    X = np.column_stack((X,tem))
+    b = np.ones(X.shape[0])
+    X = np.column_stack((X,b))
+    l = 0.5
+    a = 0.5
+    t = 100
+    w = np.zeros(10)
+    data = np.append(data,[data[0]*data[1],data[2]*data[3],data[4]*data[5],1])
+    for i in range (t):
+        s = np.zeros(10)
+        for j in range (X.shape[0]-1):
+            s = s - np.matmul(X,y-np.matmul(X,w)) + l * np.sign(w)
+        w = w - a / X.shape[0] * s
+    return w @ data
 
 
 def read_data(path='./data/exp02/'):
