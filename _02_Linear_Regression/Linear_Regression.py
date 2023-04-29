255f5e5fc1e5d4c16e9873a2bb1a218884cd2a3b
hentuatua <131958368+hentuatua@users.noreply.github.com>
2023-04-29 12:36:24 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -28,21 +28,23 @@ def ridge(data):
 def lasso(data):
     X,y = read_data()
     y =np.mat(y.reshape(-1,1))
-    tem = X [:,0] * X [:,1]
+    tem = X [:,0] * X [:,1]/2
     X = np.column_stack((X,tem))
-    tem = X [:,4] * X [:,5]
+    tem = X [:,2] * X [:,3]/2
+    X = np.column_stack((X,tem))
+    tem = X [:,4] * X [:,5]/2
     X = np.column_stack((X,tem))
     b = np.ones(X.shape[0])
     X = np.column_stack((X,b))
     m = X.shape[0]
     n = X.shape[1]
     l = 0.05
-    a = 5e-13
-    t = 20000
+    a = 1e-12
+    t = 50000
     w = np.zeros(n)
     #w=np.mat(w)
     w=w.reshape(-1,1)
-    data = np.append(data,[data[0]*data[1],data[4]*data[5],1])
+    data = np.append(data,[data[0]*data[1]/2,data[2]*data[3]/2,data[4]*data[5]/2,1])
     for i in range (t):
         gra= (np.matmul(X.T,np.matmul(X,w)-y) /n) + l * np.sign(w)
         w = w - a * gra
